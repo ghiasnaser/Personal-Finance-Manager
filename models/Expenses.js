e397262e42417736bcaaa2f645f57761bf7858d3
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-06-26 21:14:30 -0700

    creating the expenses model and adding comments on the other models

---
+++ models/Expenses.js
@@ -0,0 +1,39 @@
+// Expenses: This table tracks the expenses associated with a specific budget.
+ const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Expenses extends Model {}
+
+Expenses.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    budget_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'budget',
+          key: 'id',
+        },
+    },
+    transaction_id:{
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'transaction',
+          key: 'id',
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'expenses',
+  }
+);
+
+module.exports = Expenses;
