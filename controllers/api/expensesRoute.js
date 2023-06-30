2fbbe84fc86461ca78d219fc584a353db2b60f13
Beatriz Sandoval <beasaddress@gmail.com>
2023-06-29 21:17:09 -0700

    completed the expenses route with the functions findAll(), findByPk(), update(), create(), and destory()

--- controllers/api/expensesRoute.js
+++ controllers/api/expensesRoute.js
@@ -10,8 +10,60 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await Expenses.findByPk(req.params.id);
+        if (!userData) {
+            res.status(404).json({ message: 'No expenses with this id!' });
+            return;
+          }
+          res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
 router.post('/', async (req, res) => {
-try {
-    const userData = await Expenses.create(req.body);
-}
-})
+    try {
+        const userData = await Expenses.create(req.body);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+      const userData = await Expenses.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!userData[0]) {
+        res.status(404).json({ message: 'No expenses with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const userData = await Expenses.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!userData) {
+        res.status(404).json({ message: 'No expenses with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
